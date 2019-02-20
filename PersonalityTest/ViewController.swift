d9f02268dc0f4a1672cfc6bb2491fd686218d556
Faical1212 <scenateur_9@hotmail.com>
2019-02-20 03:53:09 -0500

    Initial Commit

---
+++ PersonalityTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityTest
+//
+//  Created by Faical sawadogo on 2/20/19.
+//  Copyright © 2019 Faical sawadogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
